d3bccd9de4da091a64f962adc246ed5c84bd8432
Anton <kam_ant1983@mail.ru>
2019-12-23 12:46:25 +0800

    add slick slider

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>'
+
+        
+      });
+  });
